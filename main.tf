45c03dac5065a7d3bf194dbd35ca856f0f53c5ce
vladc <vlad@aucsys.co.uk>
2020-06-17 18:42:50 +0100

    initial

---
+++ main.tf
@@ -0,0 +1,47 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "identity" {}
+
+module "lambda" {
+  source                       = "terraform-aws-modules/lambda/aws"
+  function_name                = var.function_name
+  handler                      = var.handler
+  runtime                      = var.runtime
+  publish                      = false
+  layers                       = var.layers
+  timeout                      = 30
+  create_async_event_config    = true
+  maximum_event_age_in_seconds = 120
+  maximum_retry_attempts       = 0
+  environment_variables        = var.env_vars
+  policies                     = concat(var.attached_policies, [aws_iam_policy.sqs_policy.arn])
+
+  #todo check this out
+  create_package         = false
+  local_existing_package = var.file_name
+}
+
+resource "aws_lambda_event_source_mapping" "event_source" {
+  event_source_arn = var.event_queue_arn
+  function_name    = module.lambda.lambda-arn
+  batch_size       = 10 # 10 is default but we set it here for clarity
+}
+
+resource "aws_iam_policy" "sqs_policy" {
+  name_prefix = var.function_name
+  policy      = data.aws_iam_policy_document.sqs_policy_doc.json
+}
+
+data "aws_iam_policy_document" "sqs_policy_doc" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      var.event_queue_arn
+    ]
+  }
+}
